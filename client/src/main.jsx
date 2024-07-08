d42285abeb46b3a134d56ae04218daa3b82c477f
sukimixcr <sukimixcr@gmail.com>
2024-07-07 20:36:37 -0700

    assignment finished

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useState, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const App = () => {
+  const [employees, setEmployees] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      const response = await axios.get("/api/employees");
+      setEmployees(response.data);
+      setIsLoading(false);
+    };
+    fetchEmployees();
+  }, []);
+  console.log(employees);
+
+  if (isLoading) {
+    return <section className="loading">Loading</section>;
+  }
+
+  return (
+    <main>
+      <article>
+        <h1>Acme HR ({employees.length})</h1>
+        <ul>
+          {employees.map((employee) => {
+            return (
+              <li key={employee.id}>
+                {employee.name}
+                {employee.is_admin ? (
+                  //   <span  style={{ paddingLeft: "1rem" }}>Administrator</span>
+                  // ) : null}
+                  <span className="admin">Administrator</span>
+                ) : null}
+              </li>
+            );
+          })}
+        </ul>
+      </article>
+    </main>
+  );
+};
+
+const root = createRoot(document.querySelector("#root"));
+
+root.render(<App />);
---
+++ server/index.js
@@ -0,0 +1,53 @@
+// static routes here (you only need these for deployment)
+
+const express = require("express");
+const app = express();
+const pg = require("pg");
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_hr_db"
+);
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", async (req, res, next) => {
+  res.send("Acme Employees");
+});
+
+app.get("/api/employees", async (req, res, next) => {
+  try {
+    const SQL = `
+          SELECT * from employees;
+        `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const init = async () => {
+  app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+  await client.connect();
+  const SQL = `
+    DROP TABLE IF EXISTS employees;
+    CREATE TABLE employees(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(50),
+      is_admin BOOLEAN DEFAULT FALSE
+    );
+    INSERT INTO employees(name, is_admin) VALUES('Mariana Vega', true);
+    INSERT INTO employees(name, is_admin) VALUES('Carlos Jimenez', true);
+    INSERT INTO employees(name) VALUES('Sonia Soto');
+    INSERT INTO employees(name) VALUES('Daniel Sanchez');
+  `;
+  await client.query(SQL);
+  console.log("data seeded");
+};
+
+init();
+
+// app routes here
+
+// create your init function
+
+// init function invocation
